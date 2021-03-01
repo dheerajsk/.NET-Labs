53b6c5d0b2db8e181882ee6e47c1759dfec55b3d
Dheeraj Kumar <dheeraj.s.roy@gmail.com>
2021-03-01 08:53:35 +0530

    Primitive Types

---
+++ BasicOfCSharp/BasicOfCSharp/PrimitiveTypes.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BasicsOfCSharp.Types
+{
+    class PrimitiveTypes
+    {
+        public static void Do()
+        {
+            bool xbool = true;
+            int xint = 100;
+            long xlong = 100000000000000;
+            short xshort = 10;
+            float xfloat = 10.05f;
+            double xdouble = 10001.897;
+            decimal xdecimal = 87.9M;
+            string xstring = "name";
+            char xchar = 'c';
+            object xobject = new object();
+
+            Console.Write(xchar);
+            Console.Write(xlong);
+            Console.WriteLine(xstring);
+            Console.WriteLine(xshort);
+        }
+    }
+}
---
+++ BasicOfCSharp/BasicOfCSharp/Program.cs
@@ -0,0 +1,14 @@
+﻿using BasicsOfCSharp.Types;
+using System;
+
+namespace BasicsOfCSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            PrimitiveTypes.Do();
+        }
+    }
+}
