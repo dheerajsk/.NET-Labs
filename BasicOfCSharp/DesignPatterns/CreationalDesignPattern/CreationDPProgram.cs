5e4cb198cb0595f0b8051a2fa3d942b0aeda24f2
Dheeraj Kumar <dheeraj.s.roy@gmail.com>
2021-03-09 11:25:22 +0530

    Singleton Pattern

--- BasicOfCSharp/DesignPatterns/CreationalDesignPattern/CreationDPProgram.cs
+++ BasicOfCSharp/DesignPatterns/CreationalDesignPattern/CreationDPProgram.cs
@@ -8,7 +8,7 @@ namespace DesignPatterns.CreationalDesignPattern
     {
         public static void Do()
         {
-            Console.WriteLine("==Abstract Factory Pattern==");
+            Console.WriteLine("== Abstract Factory Pattern ==");
             MyKitchen k = new MyKitchen();
             k.CreateItems();
             var items = k.GetItems();
@@ -16,6 +16,15 @@ namespace DesignPatterns.CreationalDesignPattern
             {
                 Console.WriteLine(item.Print());
             }
+
+            Console.WriteLine("== Singleton Pattern ==");
+            ThisApplication app1 = ThisApplication.Instance();
+            app1.SetName("App 1");
+            Console.WriteLine(app1.getName());
+            ThisApplication app2 = ThisApplication.Instance();
+            app2.SetName("App 2");
+            Console.WriteLine(app2.getName());
+            Console.WriteLine(app1.getName() + " == " + app2.getName());
         }
     }
 }
---
+++ BasicOfCSharp/DesignPatterns/CreationalDesignPattern/ThisApplication.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DesignPatterns.CreationalDesignPattern
+{
+    class ThisApplication
+    {
+        private static ThisApplication _instance;
+        private string name = "ThisApplication";
+
+        private ThisApplication()
+        {
+
+        }
+
+        public static ThisApplication Instance()
+        {
+            if (_instance == null)
+            {
+                _instance = new ThisApplication();
+            }
+            return _instance;
+        }
+
+        public string getName()
+        {
+            return name;
+        }
+
+        public void SetName(string name)
+        {
+            this.name = name;
+        }
+    }
+}
