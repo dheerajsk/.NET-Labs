4bb8ad7d2cdd684df63391fdcece59048f146d40
Dheeraj Kumar <dheeraj.s.roy@gmail.com>
2021-03-11 10:09:39 +0530

    Add NuGet package

---
+++ BasicOfCSharp/NuGetDemo/Employee.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NuGetDemo
+{
+    class Employee
+    {
+        public int ID { get; set; }
+
+        public string Name { get; set; }
+
+        public string Dept { get; set; }
+
+
+    }
+}
---
+++ BasicOfCSharp/NuGetDemo/Program.cs
@@ -0,0 +1,29 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+
+namespace NuGetDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            string data = "{\"name\":\"Tom S.\", \"age\":\"14\"}";
+            Console.WriteLine(data);
+            JArray array = new JArray();
+            JValue text = new JValue("Manual text");
+            JValue date = new JValue(new DateTime(2000, 5, 23));
+            array.Add(text);
+            array.Add(date);
+            Console.WriteLine(array.ToString());
+
+            Employee e = new Employee();
+            e.ID = 1;
+            e.Name = "Xyz";
+            e.Dept = "IT";
+
+            Console.WriteLine(JsonConvert.SerializeObject(e));
+        }
+    }
+}
